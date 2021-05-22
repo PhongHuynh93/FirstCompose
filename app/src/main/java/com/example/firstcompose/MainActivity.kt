48468d41d9df81f41cb48b3c4b9a583bc9e87080
Phong Huynh <hdthanhphong93@gmail.com>
2021-05-22 16:30:30 +0700

    Add button

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -5,10 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.Divider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -20,7 +23,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             FirstComposeTheme {
-                MyScreenContent()
+                Surface(color = Color.Yellow) {
+                    MyScreenContent()
+                }
             }
         }
     }
@@ -29,9 +34,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun Greeting(name: String) {
     // A surface container using the 'background' color from the theme
-    Surface(color = Color.Yellow) {
-        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
-    }
+    Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
 }
 
 @Composable
@@ -40,9 +43,21 @@ fun MyScreenContent() {
         Greeting("Android")
         Divider(color = Color.Black)
         Greeting("there")
+        Divider(color = Color.Black)
+        Counter()
     }
 }
 
+@Composable
+fun Counter() {
+    val count = remember { mutableStateOf(0) }
+
+    Button(onClick = { count.value++ }, modifier = Modifier.padding(24.dp)) {
+        Text("I've been clicked ${count.value} times")
+    }
+}
+
+// ############################## Preview ##############################
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
