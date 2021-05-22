843faac4022d9a4519b4595defcf2ba9df99fc65
Phong Huynh <hdthanhphong93@gmail.com>
2021-05-22 18:12:19 +0700

    Update style for button

--- app/src/main/java/com/example/firstcompose/MainActivity.kt
+++ app/src/main/java/com/example/firstcompose/MainActivity.kt
@@ -42,7 +42,8 @@ fun Greeting(name: String) {
         modifier = Modifier
             .padding(24.dp)
             .background(color = backgroundColor)
-            .clickable(onClick = { isSelected = !isSelected })
+            .clickable(onClick = { isSelected = !isSelected }),
+        style = MaterialTheme.typography.body1
     )
 }
 
